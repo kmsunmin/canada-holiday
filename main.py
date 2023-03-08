378e83d0d6138e46a06f3e5284c36051f04a4e67
Sunmin Kim <kmsunmin@gmail.com>
2023-03-07 20:09:08 -0500

    Add brute-force find_all_holidays_in_year()

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import click
+from calendar import Calendar
 import datetime
+import math
+from typing import List
 
 FIXED_DATE_HOLIDAYS = {
     # Month : Day
@@ -9,17 +11,23 @@ FIXED_DATE_HOLIDAYS = {
 }
 
 # ex: first Monday of the month
-STRIDING_HOLIDAYS = {
-    2: (3, "Mon"),  # Family Day, 3rd Monday
-    9: (1, "Mon"),  # Labour Day, 1st Monday
-    10: (2, "Mon"),  # Thanksgiving Day, 2nd Monday
+STRIDING_HOLIDAYS_ON = {
+    2: [(3, "mon", "Family Day")],  # Family Day, 3rd Monday
+    9: [(1, "mon", "Labour Day")],  # Labour Day, 1st Monday
+    10: [(2, "mon", "Thanksgiving Day")],  # Thanksgiving Day, 2nd Monday
 }
 
 # ex: last Monday preceding a certain date/holiday
-PRECEDING_HOLIDAYS = {
-    "apr": "Fri/Easter Sunday",  # Good Friday (Friday before Easter Sunday)
-    "may": "Mon/25",  # Victoria Day (Monday before May 25th)
+PRECEDING_HOLIDAYS_ON = {
+    4: [
+        ("fri", "Easter Sunday", "Good Friday")
+    ],  # Good Friday (Friday before Easter Sunday)
+    5: [("mon", 25, "Victoria Day")],  # Victoria Day (Monday before May 25th)
 }
+
+DAY_TO_INDEX = {"mon": 0, "tue": 1, "wed": 2, "thr": 3, "fri": 4, "sat": 5, "sun": 6}
+
+cal = Calendar()
 
 
 def read_holidays_tuple(
@@ -50,12 +58,95 @@ def get_fixed_date_holidays(year: int, month: int = None) -> list:
     return sorted(fixed_date_holidays)
 
 
-def get_striding_holidays(year: int, province: str):
-    pass
+def get_striding_holidays(year: int, province: str) -> List[datetime.date]:
+    """
+    Striding holidays are the ones nth day of a month
+    ex: first Monday of the month
+
+    Index of the array represents a week day
+    ex: 0 - Monday, 1 - Tuesday, 2 - Wednesday, 3 - Thursday
+        4 - Friday, 5 - Saturday, 6 - Sunday
+    """
+    striding_holidays = []
+    if province.lower() == "on":
+        for month_key in STRIDING_HOLIDAYS_ON:
+            for holiday_tuple in STRIDING_HOLIDAYS_ON[month_key]:
+                week_num, day, holiday_name = holiday_tuple
+                day_index = DAY_TO_INDEX[day]
+                str_hol = (
+                    cal.monthdatescalendar(year, month_key)[week_num][day_index],
+                    holiday_name,
+                )
+                striding_holidays.append(str_hol)
+    return striding_holidays
 
 
-def get_preceding_holidays(year: int, province: str):
-    pass
+def find_easter_day(year):
+    """
+    # Use algorithm to find the date of Easter day
+    # Reference: https://www.geeksforgeeks.org/how-to-calculate-the-easter-date-for-a-given-year-using-gauss-algorithm/
+    """
+    A = year % 19
+    B = year % 4
+    C = year % 7
+    P = math.floor(year / 100)
+    Q = math.floor((13 + 8 * P) / 25)
+    M = 15 - Q + P - (P // 4) % 30
+    N = 4 + P - (P // 4) % 7
+    D = (19 * A + M) % 30
+    E = (N + 2 * B + 4 * C + 6 * D) % 7
+    days = 22 + D + E
+
+    # A corner case: when D is 29
+    if (D == 29) and (E == 6):
+        easter_day = datetime.date(year, 4, 19)
+        return easter_day
+    # Another corner case: when D is 28
+    elif (D == 28) and (E == 6):
+        easter_day = datetime.date(year, 4, 18)
+        return easter_day
+    else:
+        # If days > 31, move to April
+        if days > 31:
+            easter_day = datetime.date(year, 4, days - 31)
+            return easter_day
+        else:
+            # Otherwise, stay on March
+            easter_day = datetime.date(year, 3, days)
+            return easter_day
+
+
+def get_preceding_holidays(year: int, province: str) -> List[datetime.date]:
+    """
+    Preceding holidays are the ones before a certain date or day
+    ex: Victoria Day is Monday before May 25th
+    """
+    preceding_holidays = []
+    if province.lower() == "on":
+        for month_key in PRECEDING_HOLIDAYS_ON:
+            for holiday_tuple in PRECEDING_HOLIDAYS_ON[month_key]:
+                day, preceding_date, holiday_name = holiday_tuple
+                day_idx = DAY_TO_INDEX[day]
+                if preceding_date == "Easter Sunday":
+                    preceding_datetime = find_easter_day(year)
+                else:
+                    preceding_datetime = datetime.date(year, month_key, preceding_date)
+
+                # Find the day before the preceding date
+                month_dates = cal.monthdatescalendar(year, month_key)
+                for week_idx, week in enumerate(month_dates):
+                    if preceding_datetime in week:
+                        # Check if preceding_date is the day or after
+                        preceding_day_idx = week.index(preceding_datetime)
+                        if day_idx < preceding_day_idx:
+                            # The holiday is in the same week
+                            pre_hol = week[day_idx]
+                        else:
+                            # The holiday is in the previous week
+                            pre_hol = month_dates[week_idx - 1]
+                        preceding_holidays.append((pre_hol, holiday_name))
+
+    return preceding_holidays
 
 
 def get_province(prov: str):
@@ -84,7 +175,7 @@ def get_province(prov: str):
 
 def find_all_holidays_in_year(year: int, prov: str):
     province = get_province(prov)
-    return (
+    return sorted(
         get_fixed_date_holidays(year)
         + get_striding_holidays(year, province)
         + get_preceding_holidays(year, province)
@@ -95,20 +186,7 @@ def find_holidays_in_year_month(year: int, month: int, province: str):
     pass
 
 
-def is_holiday(year: int, month: int, day: int, province: str):
-    pass
-
-
-@click.command()
-@click.option("-y", "--year", "year", type=int, help="Year to search")
-@click.option("-m", "--month", "month", type=int, help="Month to search")
-@click.option("-d", "--day", "day", type=int, help="Day to search")
-@click.option("-p", "--province", "province", default="ON", help="Province to search")
-def main(year, month, day, province):
-    print(f"year: {year}, month: {month}, day: {day}, province: {province}")
-    fixed_holidays = get_fixed_date_holidays(year, month)
-    print(fixed_holidays)
-
-
-if __name__ == "__main__":
-    main()
+def is_holiday(year: int, month: int, day: int, province: str) -> bool:
+    given_datetime = datetime.day(year, month, day)
+    all_holidays_year = find_all_holidays_in_year(year, province)
+    return given_datetime in all_holidays_year
