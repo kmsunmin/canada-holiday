ffa21027fc76cd7e9bd2aa59d2a2d3990dfb73ce
Sunmin Kim <kmsunmin@gmail.com>
2023-03-11 16:14:33 -0500

    Add a simple unit test for fixed holidays

---
+++ tests/test_fixed_holidays.py
@@ -0,0 +1,36 @@
+import datetime
+from typing import List
+
+from main import get_fixed_date_holidays
+
+
+EXPECTED_FIXED_DATE_HOLIDAYS = {
+    # Month : Day
+    1: [(1, "New Year's Day")],
+    7: [(1, "Canada Day")],
+    12: [(25, "Christmas Day"), (26, "Boxing Day")],
+}
+
+
+def convert_holiday_to_datetime(holidays: dict, year) -> List[datetime.date]:
+    holidays_datetime = []
+    for expected_fh_month in holidays:
+        for expected_fh_day in holidays[expected_fh_month]:
+            day, holiday_name = expected_fh_day
+            expected_hol_datetime = datetime.date(year, expected_fh_month, day)
+            holidays_datetime.append((expected_hol_datetime, holiday_name))
+    return holidays_datetime
+
+
+class TestFixedDateHolidays:
+    def test_all_fixed_date_holidays_are_returned(self):
+        """
+        For fixed date holidays, year may not matter much, but it is helpful
+        for returning a calendar date for the specific year
+        """
+        year = 2023
+
+        expected_fixed_holidays_datetime = convert_holiday_to_datetime(EXPECTED_FIXED_DATE_HOLIDAYS, year)
+        actual_fixed_date_holidays = get_fixed_date_holidays(year)
+
+        assert actual_fixed_date_holidays == expected_fixed_holidays_datetime
